44cfe89c584944f4caa5215357bc01671974eb8a
Pandu Rendradjaja <prendradjaja@gmail.com>
2017-09-02 00:58:21 +0000

    branch defaults (hardcoded)

--- stashtag.py
+++ stashtag.py
@@ -2,6 +2,13 @@
 import sys
 import subprocess
 
+# TODO not hardcoded lol
+DEFAULTS = '''
+hardcoded-defaults: fizz
+
+something: else abc
+'''
+
 def main():
     argv = sys.argv
     if '-s' in argv or '--show-defaults' in argv:
@@ -14,8 +21,9 @@ def show_branch_defaults():
     raise Exception('Not implemented')
 
 def list_stashes(hashtags_no_hash):
-    # TODO branch defaults (then ignore defaults too)
+    # TODO ignore defaults
     hashtags = ['#' + tag for tag in hashtags_no_hash]
+    hashtags.extend(get_defaults())
     for line in get_stashes():
         if all(tag in line for tag in hashtags):
             print(line)
@@ -32,6 +40,27 @@ def get_stashes():
     #     'stash #fizz',
     #     'stash',
     #     ]
+
+def get_defaults():
+    """ with # """
+    branch = (subprocess.check_output('git rev-parse --abbrev-ref HEAD'.split())
+            .decode('utf-8')
+            .strip())
+    defaults = parse_defaults(DEFAULTS)
+    if branch in defaults:
+        return defaults[branch]
+    else:
+        return []
+
+def parse_defaults(config_text):
+    nonempty = [line for line in config_text.split('\n') if line != '']
+    defaults = {}
+    for line in nonempty:
+        branch, tags_with_spaces = line.split(':')
+        tags = tags_with_spaces.split()
+        tags_with_hashes = ['#' + tag for tag in tags]
+        defaults[branch] = tags_with_hashes
+    return defaults
 
 if __name__ == '__main__':
     main()
