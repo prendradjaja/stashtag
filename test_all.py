a2f85e2a9548ca26e814bf522217ad6537a198e6
Pandu Rendradjaja <prendradjaja@gmail.com>
2017-09-09 05:36:00 +0000

    ++

--- test_all.py
+++ test_all.py
@@ -4,17 +4,8 @@ import collections
 
 import stashtag
 
-def test_print(capsys):
-    print(3)
-    out, err = capsys.readouterr()
-    assert out == "3\n"
-
-@mock.patch('git.get_branch')
-def test_branch(mock_get_branch):
-    mock_get_branch.return_value = 'foo'
-    assert stashtag.branch() == 'foo'
-
 # TODO move to testdata/fixtures/something
+# TODO stashes,config_text don't vary
 stash0 = 'stash@{0}: WIP on feature/fizz: Do the thing #fizz'
 stash1 = 'stash@{1}: WIP on feature/fizz: Buzz #fizz'
 stash2 = 'stash@{2}: On asdf: Verbose logging #debug'
@@ -22,23 +13,11 @@ stash3 = 'stash@{3}: On feature/fizz: Quiet logging #debug #fizz'
 all_stashes = [stash0, stash1, stash2, stash3]
 myconfig = ['feature/fizz: fizz']
 
-# TODO is this the right name?
 def unlines(*lines):
-    return '\n'.join(lines) + '\n'
-
-# TODO unused?
-def mytuple(attrs):
-    attrs = attrs.split(',')
-    def constructor(**kwargs):
-        ret = []
-        for each in attrs:
-            ret.append(kwargs[each])
-        return ret
-    return constructor
+    return ''.join(line + '\n' for line in lines)
 
 P = collections.namedtuple('P', 'branch,stashes,config_text,args,expected')
 
-# TODO stashes,config_text don't vary
 # TODO maybe don't use parametrize -- makes it hard to see which test failed
 # for something like this
 @pytest.mark.parametrize('branch,stashes,config_text,args,expected', [
@@ -75,7 +54,7 @@ P = collections.namedtuple('P', 'branch,stashes,config_text,args,expected')
         stashes=all_stashes,
         config_text=myconfig,
         args=['-l'],
-        expected=''  # TODO is this unlines-able
+        expected=unlines()
     ),
     P(
         branch='feature/fizz',
